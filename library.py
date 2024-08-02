791ad0ba64d346a67964c5d9211022f23f4b93b7
Charles Agoe <agoetee@gmail.com>
2024-08-02 20:16:25 +0000

    add python imports and db connection

--- library.py
+++ library.py
@@ -1,1 +1,14 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
 
+load_dotenv()
+
+myConn = mysql.connector.connect(
+    host = os.getenv("DB_HOST"),
+    username = os.getenv("DB_USERNAME"),
+    passwd = os.getenv("DB_PASSWORD"),
+    database = os.getenv("DB_DATABASE")
+)
+
+myCursor = myConn.cursor()
